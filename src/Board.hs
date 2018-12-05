9ce304b4c967c2eac7b6770a57c5a88c405fbbe4
aleonal <thelifeofantoine@outlook.com>
2018-12-05 12:24:54 -0700

    Completed 1 and 2 in part I, bug in dropInCol.

--- src/Board.hs
+++ src/Board.hs
@@ -2,7 +2,7 @@ import Data.List
 
 -- ---------- CREATING A BOARD AND PLAYERS ----------
 
--- helper, builds list of length n
+-- helper, builds list of length n, effectively a connect 4 column
 mkRow :: Int -> [Int]
 mkRow 0 = []
 mkRow n = 0 : mkRow(n - 1)
@@ -18,13 +18,39 @@ mkOpponent = 2
 
 -- ---------- CHECKING A BOARD AND DROPING A DISC ----------
 
--- helper, checks if there is an open slot (denoted by a 0) in the given list
+-- helper, checks if there is an open slot (denoted by a 0) in the given column
 isColOpen :: [Int] -> Bool
 isColOpen [] = False
 isColOpen (x : xs) = if x == 0 then True else isColOpen xs
 
--- function that checks for an open slot in n-th column of the given m x n list
+-- helper, drops player's checker in given column
+dropInCol :: [Int] -> Int -> [Int]
+dropInCol [] p = []
+dropInCol (x : xs) p = if last (x : xs) == 0 then
+  reverse (p : reverse (init (x : xs))) else
+  reverse (last (x : xs) : reverse (dropInCol (init (x : xs)) p))
+
+-- function that checks for an open slot in n-th column of the given board
 isSlotOpen :: [[Int]] -> Int -> Bool
 isSlotOpen [] n = False
 isSlotOpen (x : xs) 1 = isColOpen x
 isSlotOpen (x : xs) n = isSlotOpen xs (n - 1)
+
+-- function that returns the number of columns in the given board
+numSlot :: [[Int]] -> Int
+numSlot [] = 0
+numSlot (x : xs) = 1 + numSlot xs
+
+-- function that determines whether the given board is full or not
+isFull :: [[Int]] -> Bool
+isFull [] = False
+isFull (x : xs) =  not (isColOpen x) && isFull xs
+
+-- function that drops a player p's disc in a slot.
+dropInSlot :: [[Int]] -> Int -> Int -> [[Int]]
+dropInSlot [] i p = []
+dropInSlot (x : xs) i p = if isColOpen ((x : xs) !! i) then
+  reverse (dropInCol ((x : xs) !! i) p : reverse (take (i - 1) (x : xs))) ++
+  drop i (x : xs) else (x : xs)
+
+-- ---------- DETERMINING THE OUTCOMEC ----------
